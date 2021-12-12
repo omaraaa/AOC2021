1b480383230f025b9fe4945e6ee124d3e4a89dde
Omar A. Alhammadi <emarati.no1@gmail.com>
2021-12-12 23:12:52 +0400

    refactored Day 12 solution to be a little nicer

--- d12/main.zig
+++ d12/main.zig
@@ -2,21 +2,23 @@ const std = @import("std");
 var gpa = std.heap.GeneralPurposeAllocator(.{}){};
 
 const N = usize;
-const NCount = 16;
+
+const NODE_COUNT = 16;
+const NODE_TYPE = u4;
 
 const State = struct {
     const Self = @This();
 
-    name_index: u8 = 0,
-    start: u8 = 0,
-    end: u8 = 0,
-    graph: [NCount][NCount]bool = std.mem.zeroes([NCount][NCount]bool),
-    node_small: [NCount]bool = std.mem.zeroes([NCount]bool),
-    name_map: std.StringHashMap(u8),
+    name_index: NODE_TYPE = 0,
+    start: NODE_TYPE = 0,
+    end: NODE_TYPE = 0,
+    graph: [NODE_COUNT][NODE_COUNT]bool = std.mem.zeroes([NODE_COUNT][NODE_COUNT]bool),
+    node_small: [NODE_COUNT]bool = std.mem.zeroes([NODE_COUNT]bool),
+    name_map: std.StringHashMap(NODE_TYPE),
 
     pub fn init() Self {
         return Self{
-            .name_map = std.StringHashMap(u8).init(&gpa.allocator),
+            .name_map = std.StringHashMap(NODE_TYPE).init(&gpa.allocator),
         };
     }
     pub fn deinit(self: *Self) void {
@@ -31,7 +33,7 @@ const State = struct {
         self.graph[id2][id1] = true;
     }
 
-    pub fn toID(self: *Self, n: []const u8) !u8 {
+    pub fn toID(self: *Self, n: []const u8) !NODE_TYPE {
         var n_id = self.name_map.get(n);
         if (n_id == null) {
             if (std.mem.eql(u8, n, "start")) {
@@ -57,189 +59,99 @@ const State = struct {
         return n_id.?;
     }
 
-    pub fn pathItr1(self: *Self) PathIterator1 {
-        return PathIterator1{
+    const Part = enum { part1, part2 };
+    pub fn pathItr(self: *Self, comptime part: Part) PathIterator(part) {
+        return PathIterator(part){
             .state = self,
         };
     }
 
-    pub fn pathItr2(self: *Self) PathIterator2 {
-        return PathIterator2{
-            .state = self,
+    const PathStack = struct {
+        nodes: [NODE_COUNT]NODE_TYPE = undefined,
+        len: usize = 0,
+        itr: usize = 0,
+
+        pub fn append(self: *@This(), node: NODE_TYPE) !void {
+            self.nodes[self.len] = node;
+            self.len += 1;
+        }
+
+        pub fn next(self: *@This()) ?NODE_TYPE {
+            self.itr += 1;
+            if (self.itr <= self.len) {
+                return self.nodes[self.itr - 1];
+            } else {
+                return null;
+            }
+        }
+    };
+
+    pub fn PathIterator(comptime part: Part) type {
+        return struct {
+            started: bool = false,
+            state: *Self,
+            path_found: bool = false,
+            stack: std.ArrayList(PathStack) = undefined,
+
+            pub fn next(self: *@This()) !bool {
+                if (!self.started) {
+                    self.stack = std.ArrayList(PathStack).init(&gpa.allocator);
+                    var stack = PathStack{};
+                    try stack.append(self.state.start);
+                    try self.stack.append(stack);
+                    self.started = true;
+                }
+                try self.path();
+                if (!self.path_found) {
+                    self.stack.deinit();
+                }
+                return self.path_found;
+            }
+
+            fn path(self: *@This()) anyerror!void {
+                while (self.stack.items.len > 0) {
+                    var stack = &self.stack.items[self.stack.items.len - 1];
+                    var next_node = stack.next();
+                    if (next_node == null) {
+                        _ = self.stack.pop();
+                        continue;
+                    }
+                    var node = next_node.?;
+
+                    if (node == self.state.end) {
+                        self.path_found = true;
+                        return;
+                    }
+
+                    var ps = PathStack{};
+                    var visit_count: [NODE_COUNT]NODE_TYPE = std.mem.zeroes([NODE_COUNT]NODE_TYPE);
+                    var twice: bool = false;
+
+                    for (self.stack.items) |s| {
+                        var n = s.nodes[s.itr - 1];
+                        if (self.state.node_small[n] and visit_count[n] < 2) {
+                            visit_count[n] += 1;
+                            if (twice)
+                                visit_count[n] += 1;
+                        }
+                        if (visit_count[n] == 2 and n != self.state.start and n != self.state.end) {
+                            twice = true;
+                        }
+                    }
+
+                    var cmp: u2 = if (twice or part == .part1) 1 else 2;
+                    for (self.state.graph[node]) |e, n| {
+                        var nn = @intCast(NODE_TYPE, n);
+                        if (e and visit_count[nn] < cmp and nn != self.state.start) {
+                            try ps.append(nn);
+                        }
+                    }
+                    try self.stack.append(ps);
+                }
+                self.path_found = false;
+            }
         };
     }
-
-    pub const PathIterator1 = struct {
-        const PathStack = struct {
-            nodes: [NCount]u8 = undefined,
-            len: usize = 0,
-            itr: usize = 0,
-
-            pub fn append(self: *@This(), node: u8) !void {
-                self.nodes[self.len] = node;
-                self.len += 1;
-            }
-
-            pub fn next(self: *@This()) ?u8 {
-                self.itr += 1;
-                if (self.itr <= self.len) {
-                    return self.nodes[self.itr - 1];
-                } else {
-                    return null;
-                }
-            }
-        };
-
-        started: bool = false,
-        state: *Self,
-        path_found: bool = false,
-        visited: std.AutoHashMap(u8, void) = undefined,
-        to_visit: std.ArrayList(PathStack) = undefined,
-
-        pub fn next(self: *@This()) !bool {
-            if (!self.started) {
-                self.visited = std.AutoHashMap(u8, void).init(&gpa.allocator);
-
-                self.to_visit = std.ArrayList(PathStack).init(&gpa.allocator);
-                var stack = PathStack{};
-                try stack.append(self.state.start);
-                try self.to_visit.append(stack);
-                self.started = true;
-            }
-            try self.path();
-            if (!self.path_found) {
-                self.visited.deinit();
-                self.to_visit.deinit();
-            }
-            return self.path_found;
-        }
-
-        fn path(self: *@This()) anyerror!void {
-            while (self.to_visit.items.len > 0) {
-                var stack = &self.to_visit.items[self.to_visit.items.len - 1];
-                var next_node = stack.next();
-                if (next_node == null) {
-                    for (stack.nodes[0..stack.len]) |n| {
-                        _ = self.visited.remove(n);
-                    }
-                    _ = self.to_visit.pop();
-                    continue;
-                }
-
-                var node = next_node.?;
-                for (stack.nodes[0..stack.itr]) |n| {
-                    _ = self.visited.remove(n);
-                }
-
-                if (self.state.node_small[node]) {
-                    try self.visited.put(node, {});
-                }
-
-                if (node == self.state.end) {
-                    self.path_found = true;
-
-                    return;
-                }
-
-                var ps = PathStack{};
-                for (self.state.graph[node]) |e, n| {
-                    var nn = @intCast(u8, n);
-                    if (e and self.visited.get(nn) == null) {
-                        try ps.append(nn);
-                    }
-                }
-                try self.to_visit.append(ps);
-            }
-            self.path_found = false;
-        }
-    };
-
-    pub const PathIterator2 = struct {
-        const PathStack = struct {
-            nodes: [NCount]u8 = undefined,
-            visit_count: [NCount]u8 = std.mem.zeroes([NCount]u8),
-            twice: bool = false,
-            len: usize = 0,
-            itr: usize = 0,
-
-            pub fn append(self: *@This(), node: u8) !void {
-                self.nodes[self.len] = node;
-                self.len += 1;
-            }
-
-            pub fn next(self: *@This()) ?u8 {
-                self.itr += 1;
-                if (self.itr <= self.len) {
-                    return self.nodes[self.itr - 1];
-                } else {
-                    return null;
-                }
-            }
-        };
-
-        started: bool = false,
-        state: *Self,
-        path_found: bool = false,
-        visited: [256]u2 = std.mem.zeroes([256]u2),
-        to_visit: std.ArrayList(PathStack) = undefined,
-
-        pub fn next(self: *@This()) !bool {
-            if (!self.started) {
-                self.to_visit = std.ArrayList(PathStack).init(&gpa.allocator);
-                var stack = PathStack{};
-                try stack.append(self.state.start);
-                try self.to_visit.append(stack);
-                self.started = true;
-            }
-            try self.path();
-            if (!self.path_found) {
-                self.to_visit.deinit();
-            }
-            return self.path_found;
-        }
-
-        fn path(self: *@This()) anyerror!void {
-            while (self.to_visit.items.len > 0) {
-                var stack = &self.to_visit.items[self.to_visit.items.len - 1];
-                var next_node = stack.next();
-                if (next_node == null) {
-                    _ = self.to_visit.pop();
-                    continue;
-                }
-
-                var node = next_node.?;
-
-                if (node == self.state.end) {
-                    self.path_found = true;
-                    stack.visit_count[node] = 2;
-
-                    return;
-                }
-
-                var ps = PathStack{};
-                for (self.to_visit.items) |s| {
-                    var n = s.nodes[s.itr - 1];
-                    if (self.state.node_small[n] and ps.visit_count[n] < 2) {
-                        ps.visit_count[n] += 1;
-                        if (ps.twice)
-                            ps.visit_count[n] += 1;
-                    }
-                    if (ps.visit_count[n] == 2 and n != self.state.start and n != self.state.end) {
-                        ps.twice = true;
-                    }
-                }
-                var cmp: u2 = if (ps.twice) 1 else 2;
-                for (self.state.graph[node]) |e, n| {
-                    var nn = @intCast(u8, n);
-                    if (e and ps.visit_count[nn] < cmp and nn != self.state.start) {
-                        try ps.append(nn);
-                    }
-                }
-                try self.to_visit.append(ps);
-            }
-            self.path_found = false;
-        }
-    };
 };
 
 fn solution(buf: []const u8) ![2]N {
@@ -257,7 +169,7 @@ fn solution(buf: []const u8) ![2]N {
     }
 
     {
-        var itr = state.pathItr1();
+        var itr = state.pathItr(.part1);
         var path_count: usize = 0;
         while (try itr.next()) {
             path_count += 1;
@@ -266,7 +178,7 @@ fn solution(buf: []const u8) ![2]N {
     }
 
     {
-        var itr = state.pathItr2();
+        var itr = state.pathItr(.part2);
         var path_count: usize = 0;
         while (try itr.next()) {
             path_count += 1;
